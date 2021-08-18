6274b0fe35453bb4347aca9138c586d96341ccd2
nsinghROLO <nsingh@rodeologistics.co>
2021-08-18 03:15:24 -0500

    Added puppeteer E2E testing.

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate } = require("./util");
 
 test("should output name and age", () => {
@@ -16,3 +17,22 @@ test("should generate a valid text output", () => {
   const text = checkAndGenerate("Max", 29);
   expect(text).toBe("Max (29 years old)");
 });
+
+// E2E testing with puppeteer
+test("should create  an element with text and correct class", async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ["--window-size=1920, 1080"],
+  });
+
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000/index.html");
+  await page.click("input#name");
+  await page.type("input#name", "Anna");
+  await page.click("input#age");
+  await page.type("input#age", "29");
+  await page.click("#btnAddUser");
+  const finalText = await page.$eval(".user-item", (el) => el.textContent);
+  expect(finalText).toBe("Anna (29 years old)");
+}, 10000);
